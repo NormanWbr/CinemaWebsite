dedb94687472be40183dbd2af8543be5a3a46f22
Norman <norman@wamberchies.be>
2023-05-10 16:02:08 +0200

    update

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,12 @@
+export interface MovieModel{
+    id: number;
+    title: string;
+    director: string[];
+    genres: string[];
+    classifications: string[];
+    description: string;
+    duration: number;
+    releaseDate: string;
+    endDate: string;
+    poster: string;
+}
